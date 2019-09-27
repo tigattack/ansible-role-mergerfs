57a64f3c7b329df99aea17f1851b8568aca28d36
Sylvain Prat <sylvain.prat@gmail.com>
2019-09-27 08:48:48 +0200

    Changed the mergerfs mount path

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,7 +8,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_mount_point(host):
-    mount_point = host.mount_point('/mnt/data')
+    mount_point = host.mount_point('/mnt/storage')
     assert mount_point.exists
     assert mount_point.filesystem == 'fuse.mergerfs'
     assert 'allow_other' in mount_point.options
@@ -16,6 +16,6 @@ def test_mount_point(host):
 
 
 def test_data_files(host):
-    assert host.file('/mnt/data/file1.txt').exists
-    assert host.file('/mnt/data/file2.txt').exists
-    assert host.file('/mnt/data/file3.txt').exists
+    assert host.file('/mnt/storage/file1.txt').exists
+    assert host.file('/mnt/storage/file2.txt').exists
+    assert host.file('/mnt/storage/file3.txt').exists
